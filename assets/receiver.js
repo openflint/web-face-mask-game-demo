c1ef7795a71ad9bbdc584656d64e2f1862e7c262
tianye <tianye@infthink.com>
2014-11-12 16:20:10 +0800

    update

--- assets/receiver.js
+++ assets/receiver.js
@@ -1,1 +1,180 @@
 "use strict"
+/*
+* important set faceplusplus us api url
+**/
+
+// IDEL / SET_IMAGE / IMAGE_DETECTING / NO_FACE / FACE_FOUND
+var gameStatus = "IDLE";
+function imageDataFormat(imgData){
+    var data = atob(imgData.substring( "data:image/png;base64,".length ));
+    var asArray = new Uint8Array(data.length);
+    for(var i = 0, len = data.length; i < len; ++i){
+        asArray[i] = data.charCodeAt(i);    
+    }
+    var blob = new Blob([ asArray.buffer ], {type: "image/png"});
+    return blob;
+}
+var FaceppSDK = function(){
+    var self = this;
+    var apiKey = "fc9ee3d9b48ed027a7cedf27a8fe2434",
+        apiSecret = "3cnNalC9OF09E58R0jf4Cpu42t0WJBGR";
+
+    apiKey = "9324fbaf5b1a3e206c7ed4171b0ff469";
+    apiSecret = "pRwuUwQybWKvRN9eaRSoAHXW23aT7u4k";
+
+    self.result = null;
+    self.detect = function(imgdata){
+        var api = new FacePP(apiKey, apiSecret);
+        api.request("detection/detect", {
+            img: imgdata
+
+        }, function(err, result){
+            if (err) {
+                console.info(err);
+                return;
+            }
+            ("onresult" in self)&& (self.onresult(result));
+        });
+    };
+
+    self.on = function(type, func){
+        self["on"+type] = func;
+    };
+}
+var faceppSDK = new FaceppSDK();
+
+var AlertBox = function(){
+    self = this;
+    var box = document.getElementById("alert"),
+        alertText = document.getElementById("alert-text");
+
+    self.show = function(text){
+        box.className = "alert show";
+        alertText.innerHTML = text;
+        setTimeout(function(){
+            self.hide();
+        },5000);
+    };
+
+    self.hide = function(text){
+        box.className = "alert hide";
+    }
+};
+var alertBox = new AlertBox();
+
+var Mask = function(){
+    var self = this;
+    var img = document.getElementById("image"),
+        imgEyeLeft = new Image(),
+        imgEyeRight = new Image(),
+        canvas = document.getElementById("canvas");
+
+    var ctx = canvas.getContext("2d");
+
+    var imgBlob = null;
+    img.onload = function(){
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        canvas.style.marginLeft= "-"+(canvas.width/2)+"px";
+        canvas.className = "show";
+        alertBox.show("Detecting Face...");
+        gameStatus = "IMAGE_DETECTING";
+        faceppSDK.detect(imgBlob);
+    };
+    self.setImage = function(imgData){
+        gameStatus = "SET_IMAGE";
+        img.src = imgData;
+        imgBlob = imageDataFormat(imgData);
+    };
+
+    self.setBackground = function(result){
+        if(typeof(result)=="undefined"){
+            document.body.className = "bg-no";
+        }else if(result.face.length>1){
+            document.body.className = "bg-all";
+        }else{
+            if(result.face[0].attribute.gender.value=="Male"){
+                var num = Math.floor(Math.random()*5)+1;
+                document.body.className = "bg-m-"+num;
+            }else{
+                var num = Math.floor(Math.random()*3)+1;
+                document.body.className = "bg-f-"+num;
+            }
+        }
+    };
+
+    self.wear = function(result, maskId){
+        if(result.face.length==0){
+            self.setBackground();
+            alertBox.show("No Face found!");
+        }else{
+            alertBox.show("Face found");
+            self.setBackground(result);
+            if(typeof(maskId)=="undefined"){
+                var num = Math.floor(Math.random()*18)+1;
+                maskId = num;
+            }
+            imgEyeLeft.src = "assets/imgs/eyes/"+maskId+"-L.png";
+            imgEyeRight.src = "assets/imgs/eyes/"+maskId+"-R.png";
+
+            for(var i=0; i<result.face.length; i++){
+                var face = result.face[i];
+                var faceWidth = face.position.width*result.img_width/100,
+                    faceHeight = face.position.height*result.img_height/100;
+                var ew = faceWidth*0.4,
+                    eh = faceHeight*0.4;
+                if(ew<80){
+                    imgEyeLeft.width = ew;
+                    imgEyeLeft.height = eh;
+
+                    imgEyeRight.width = ew;
+                    imgEyeRight.height = eh;
+                }
+                console.info("---------------------->----------->", faceWidth, faceHeight, imgEyeLeft.width, imgEyeLeft.height);
+                var eyePosition = {};
+                    eyePosition.leftX = face.position.eye_left.x*result.img_width/100 - imgEyeLeft.width/2;
+                    eyePosition.leftY = face.position.eye_left.y*result.img_height/100 - imgEyeLeft.height/2;
+
+                    eyePosition.rightX = face.position.eye_right.x*result.img_width/100 - imgEyeRight.width/2;
+                    eyePosition.rightY = face.position.eye_right.y*result.img_height/100 - imgEyeRight.height/2;
+                    setTimeout(function(){
+                        ctx.drawImage(imgEyeLeft, eyePosition.leftX, eyePosition.leftY, imgEyeLeft.width, imgEyeLeft.height);
+                        ctx.drawImage(imgEyeRight, eyePosition.rightX, eyePosition.rightY, imgEyeRight.width, imgEyeRight.height);
+                    },100);
+            }
+        }
+    };
+
+    self.on = function(type, func){
+        self["on"+type] = func;
+    };
+};
+var mask = new Mask();
+
+faceppSDK.on("result", function(result){
+    console.info("------------------>result: ", result);
+    self.result = result;
+    if(result.face.length==0){
+        gameStatus = "NO_FACE";
+    }else{
+        gameStatus = "FACE_FOUND";
+    }
+});
+
+
+var wsServer = "ws://127.0.0.1:9431/receiver";
+var ws = new WebSocket(wsServer);
+ws.onopen = function (evt) {
+    console.info("--------->receiver connected");
+}; 
+
+ws.onmessage = function(evt) {
+  var msg = JSON.parse(evt.data);
+    if(msg["type"]=="img"){
+        console.info("---------------->on message: ", evt.data);
+        mask.setImage(msg["data"]);
+    }else if(msg["type"]=="mask"){
+        mask.wear(faceppSDK.result, msg["data"]["maskId"]);
+    }
+};
--- assets/sender.js
+++ assets/sender.js
@@ -1,29 +1,63 @@
 "use strict"
 
-var wsServer = "ws://127.0.0.1:9431/sender";
-var ws = new WebSocket(wsServer);
-ws.onopen = function (evt) {
-    console.info("--------->sender connected");
-}; 
-ws.onclose = function (evt) { 
-}; 
-ws.onmessage = function (evt) { 
-}; 
-ws.onerror = function (evt) {
+if (typeof String.prototype.replaceAll != 'function') {
+    String.prototype.replaceAll = function (AFindText,ARepText){
+        var raRegExp = new RegExp(AFindText,"g");
+        return this.replace(raRegExp,ARepText);
+    }
+}
 
+function getElementsByClass(searchClass, tagName) { 
+    var domNode = document;
+    if (tagName == null){
+        tagName = '*';   
+    }
+    var el = new Array();
+    var tags = domNode.getElementsByTagName(tagName);
+    var tcl = " "+searchClass+" ";
+    for(var i=0,j=0; i<tags.length; i++) { 
+        var test = " " + tags[i].className + " ";
+        if (test.indexOf(tcl) != -1){
+            el[j++] = tags[i];
+        }
+    } 
+    return el;
 };
 
-var Camera = function(){
+var appUrl = "http://openflint.github.io/web-face-mask-game-demo/receiver.html";
+
+var isFirefoxOs = typeof(MozActivity)=="undefined"?false:true;
+var masks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+function imageEncode(img, imgw, imgh){
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+    if(typeof(imgw)=="undefined"){
+        imgw = 640;
+    }
+    if(typeof(imgh)=="undefined"){
+        imgh = 480;
+    }
+    canvas.width = imgw;
+    canvas.height = imgh;
+    context.drawImage(img, 0, 0, imgw, imgh);
+    var dataURL = canvas.toDataURL("image/png");
+    console.info(img.width, img.height);
+    return {
+        'type' : 'img',
+        'data' : dataURL,
+    };
+}
+
+/*
+* Camera for browser
+**/
+var BrowserCamera = function(){
     var self = this;
     var video = document.getElementById("video"),
-        canvas = document.createElement("canvas"),
-        btnCapture = document.getElementById("btn-capture"),
+        btnCapture = document.getElementById("btn-capture-browser"),
         camera_stream = null,
         videoObj = { "video": true };
-
-    canvas.width = video.width;
-    canvas.height = video.height;
-    var context = canvas.getContext("2d");
 
     navigator.getMedia = ( navigator.getUserMedia ||
                navigator.webkitGetUserMedia ||
@@ -45,15 +79,10 @@ var Camera = function(){
     };
 
     self.snap = function(){
-        context.drawImage(video, 0, 0);
-        var dataURL = canvas.toDataURL("image/png");
-
-        var data = {
-            'type' : 'img',
-            'data' : dataURL,
-        }
-        console.info(data);
-        ws.send(JSON.stringify(data));
+        // todo
+        var data = imageEncode(video);
+        console.info(JSON.stringify(data));
+        // ws.send(JSON.stringify(data));
 
         self.stop();
     }
@@ -64,6 +93,147 @@ var Camera = function(){
     };
 };
 
+var PhoneCamera = function(){
+    var self = this;
+
+    var btnCapture = document.getElementById("btn-capture-ffphone");
+
+    btnCapture.onclick = function(){
+        var pick = new MozActivity({
+           name: "pick",
+           data: {
+               type: ["image/png", "image/jpg", "image/jpeg"]
+           }
+        });
+
+        pick.onsuccess = function () {
+            var img = document.createElement("img"),
+                pixArea = document.getElementById("pix-area"),
+                ffphoneCapture = document.getElementById("ffphone-capture");
+            console.info(this.result.blob);
+            img.src = window.URL.createObjectURL(this.result.blob);
+            img.width = 320;
+            img.height = 435;
+            
+            pixArea.appendChild(img);
+            
+            var data = imageEncode(img);
+            
+            // todo
+            // ws.send(JSON.stringify(data));
+        };
+        pick.onerror = function () {
+            console.info("Can't view the image!");
+        };
+    };
+};
+
+var GamePageView = function(){
+    var self = this;
+    var browserCapture = document.getElementById("browser-capture"),
+        ffphoneCapture = document.getElementById("ffphone-capture"),
+    
+        eleMasks = document.getElementById("masks"),
+        temp = document.getElementById("temp-mask").innerHTML,
+        masksInner = document.getElementById("masks-inner");
+
+    self.init = function(){
+        eleMasks.style.width = window.innerWidth+"px";
+
+        var html = "";
+        for (var i = masks.length - 1; i >= 0; i--) {
+            html += temp.replaceAll("##id##", masks[i]);
+        };
+        masksInner.innerHTML = html;
+
+        if(isFirefoxOs){
+            ffphoneCapture.className = "capture-area";
+            ffphoneCapture.style.width = window.innerWidth+"px";
+            ffphoneCapture.style.height = window.innerHeight-eleMasks.offsetHeight-20+"px";
+        }else{
+            var video = document.getElementById("video");
+            browserCapture.className = "capture-area";
+            var vSize = 0,
+                vHeight = window.innerHeight-eleMasks.offsetHeight-20;
+            if(window.innerWidth<vHeight){
+                vSize = window.innerWidth;
+            }else{
+                vSize = vHeight;
+            }
+            // video.style.width = vSize+"px";
+            // video.style.height = vSize+"px";
+            video.style.width = window.innerWidth+"px";
+            video.style.height = vSize+"px";
+        }
+    };
+};
+
+function showAlert(msg){
+    var errbox = document.getElementById("error"),
+        bg = document.getElementById("error-bg"),
+        msgbox = document.getElementById("error-content"),
+        text = document.getElementById("error-text");
+    text.innerHTML = msg;
+    msgbox.style.left = (window.windowWidth-250)/2+"px";
+    msgbox.style.top = (window.windowHeight-50)/2+"px";
+
+    errbox.style.display = "block";
+}
+function hideAlert(){
+    var bg = document.getElementById("error-bg"),
+        errbox = document.getElementById("error"),
+        text = document.getElementById("error-text");
+    errbox.onclick = function(){
+        text.innerHTML = "";
+        errbox.style.display = "none";
+    };
+    bg.onclick = function(){
+        text.innerHTML = "";
+        errbox.style.display = "none";
+    };
+}
+
 window.onload = function(){
-    var camera = new Camera();
+    window.windowWidth = document.body.offsetWidth;
+    window.windowHeight = window.innerHeight;
+
+    var gamePageView = new GamePageView();
+
+    gamePageView.init();
+    if(isFirefoxOs){
+        var camera = new PhoneCamera();
+    }else{
+        var camera = new BrowserCamera();
+    }
+
+    hideAlert();
+    var eleOpenBtn = document.getElementById("open-btn"),
+        eleDongleIpInput = document.getElementById("dongle-ip-input");
+    eleDongleIpInput.focus();
+    function openApp(){
+        console.info("-------------------------------openApp--------------------------");
+        if(eleDongleIpInput.value!=""){
+            var patrn =/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+            if(!patrn.exec(eleDongleIpInput.value) ){ 
+                showAlert("IP address error");
+                return;
+            }
+            // I can not get CROS error. so you must confirmed the ip address right
+            var deviceIp = eleDongleIpInput.value;
+            window.senderDaemon = new SenderDaemon(deviceIp, "~browser");
+            // communicate();
+            window.senderDaemon.openApp(appUrl, -1, true);
+            return;
+        }else{
+            showAlert("IP address error");
+        }
+    }
+    eleOpenBtn.onclick = function(e){
+        openApp();
+    };
+    eleDongleIpInput.onkeyup = function(e){
+        if(e.keyCode==13){
+            openApp();
+        }
+    };
 };
